0b81217f4e23520f88a42e74a9d3aaf719da40d2
SaySmokeGraf <seme4ka.elizovo@gmail.com>
2025-12-04 10:59:45 +0300

    Правки в конфиг

--- src/config.py
+++ src/config.py
@@ -3,23 +3,29 @@
 from math import tan, pi
 
 
+# разрешение
 H_RES = 600             # [пиксели]
 V_RES = 400             # [пиксели]
+
+# отрисовка
 FPS = 60                # [1/с]
 L_RENDER = 5            # [клетки]
 STEPS_PER_UNIT = 20     # [шаги/клетка]
-
 FOV = 90                # [градусы]
 VFOV = 60               # [градусы]
+
+# параметры игрока
 PHI_START = 0           # [градусы]
 PLAYER_SPEED = 1.0      # [клетки/с]
 PLAYER_CAM_SPEED = 90   # [градусы/с]
 
+# пути
 PATH_ICON = 'res/icon.png'
 PATH_MAP = 'res/map.txt'
 
 
 class Direct():
+    """Константы углов в градусах по направлениям."""
     FRONT = 0
     LEFT = 90
     RIGHT = -90
