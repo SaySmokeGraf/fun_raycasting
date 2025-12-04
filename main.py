1d024660e137c48b6a8915c5b20fd80c23c86b41
SaySmokeGraf <seme4ka.elizovo@gmail.com>
2025-12-04 11:42:42 +0300

    Правки в оформление выражений и перемещение конфига

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 
 from math import floor
 
-from src.config import *
+from config import *
 from src.objects import *
 
 
@@ -56,38 +56,39 @@ class App:
     
     def _upd_screen(self):
         pygame.draw.rect(self.screen, (0, 0, 0), (0, 0, H_RES, V_RES))
-        one_step = FOV/H_RES
-        max_angle = self.player.phi + FOV/2
-        floor_x_old, floor_y_old, _ = self._send_ray(x=self.player.x, y=self.player.y,
-                                           phi=max_angle - 0*one_step)
+        one_step = FOV / H_RES
+        max_angle = self.player.phi + FOV / 2
+        floor_x_old, floor_y_old, _ = self._send_ray(self.player.x,
+                                            self.player.y,
+                                            phi=max_angle - 0 * one_step)
         for i in range(H_RES):
-            floor_x, floor_y, r = self._send_ray(x=self.player.x, y=self.player.y, 
-                            phi=max_angle - i*one_step)
+            floor_x, floor_y, r = self._send_ray(self.player.x, self.player.y, 
+                                                 max_angle - i * one_step)
             
             if floor_x != floor_x_old or floor_y != floor_y_old:
                 line_color = (0, 0, 0)
                 floor_x_old = floor_x
                 floor_y_old = floor_y
             else:
-                c_temp = floor(255 - 255*r/L_RENDER)
+                c_temp = floor(255 - 255 * r / L_RENDER)
                 line_color = (c_temp, c_temp, c_temp)
             
             if r == 0:
                 coeff = 1
             else:
-                coeff = COEFF_TAN/r
+                coeff = COEFF_TAN / r
                 if coeff > 1: coeff = 1
-            line_len = coeff*V_RES
+            line_len = coeff * V_RES
 
             pygame.draw.aaline(self.screen, line_color, 
-                               (i, int((V_RES - line_len)/2)),
-                               (i, int((V_RES + line_len)/2)))
+                               (i, int((V_RES - line_len) / 2)),
+                               (i, int((V_RES + line_len) / 2)))
             
         pygame.display.update()
 
     def _send_ray(self, x, y, phi):
-        one_step_x = RAY_ONE_STEP*cos(phi*pi/180)
-        one_step_y = RAY_ONE_STEP*sin(phi*pi/180)
+        one_step_x = RAY_ONE_STEP * cos(phi * pi / 180)
+        one_step_y = RAY_ONE_STEP * sin(phi * pi / 180)
 
         for r in LIST_DISTS:
             x += one_step_x
